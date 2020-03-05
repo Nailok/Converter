1c6864dee1827ca00992017c3946b7b0dbea7a5a
Nailok <lexitor34@gmail.com>
2020-03-06 00:44:20 +0300

    Added auto converting attributes

--- converter.rb
+++ converter.rb
@@ -18,7 +18,8 @@ class Parser
         args.name = i
       end
 
-      opts.on('-o', '--output=FORMAT', 'Choose one of formats: json, atom, rss') do |i|
+      opts.on('-o', '--output=FORMAT',
+              'Choose one of formats: json, atom, rss') do |i|
         args.name = i
       end
 
@@ -38,28 +39,31 @@ end
 
 # options = Parser.parse %w[--help]
 
-# input_file = File.open("lenta_ru-rss-news.txt", "r")
 url = 'https://lenta.ru/rss/news'
-#url = 'https://news.yandex.ru/cyber_sport.rss'
-#url = 'lenta_ru-rss-news.txt'
-
+# url = 'https://news.yandex.ru/cyber_sport.rss'
+# url = 'lenta_ru-rss-news.txt'
+# url = 'test.txt'
 doc = Nokogiri::XML(open(url))
 items = []
-#feed = []
 
-#feed_tags = doc.xpath('//*').map(&:name).each.uniq
 item_tags = doc.xpath('//item/*').map(&:name).each.uniq
-
 doc.css('item').each do |item|
   parsed_strings = {}
+  attr_names = []
   item_tags.each do |tag|
-    item_result = item.css("#{tag}").text.strip
-    parsed_strings[tag] = item_result
+  attr_hash = {}
+    if !doc.xpath("//item/#{tag}[@*]").empty?
+      attr_names = doc.xpath("//item/#{tag}/@*").map(&:name).each.uniq
+      attr_names.each do |att|
+        attr_hash[att] = item.css(tag)[0][att]
+      end
+      parsed_strings[tag] = attr_hash
+    else
+      item_result = item.css(tag.to_s).text.strip
+      parsed_strings[tag] = item_result
+   end
   end
   items.push << parsed_strings
 end
-
-
-
-
 File.open('output.json', 'w+') { |f| f.write JSON.pretty_generate(items) }
+
