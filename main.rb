942125592b8b821d079b1ebf520f0bf75ab16019
Nailok <lexitor34@gmail.com>
2020-03-18 09:55:51 +0300

    Added file writer

--- main.rb
+++ main.rb
@@ -8,6 +8,7 @@ class Main
     @input = options[:input]
     @output = options[:output]
     @sort = options[:sort]
+    @name = options[:name]
   end
 
   def run
@@ -16,19 +17,9 @@ class Main
            else
              File_reader.read(@input)
            end
+
     parsed_data = BaseParser.parse(data)
-    BaseConverter.convert(parsed_data, @output)
-
-
-    #if @output.to_s.downcase == 'atom'
-      #ConvertToAtom.convert(parsed_data)
-      #p 'Output is in atom format'
-    #elsif @output.to_s.downcase == 'rss'
-      #ConvertToRss.convert(parsed_data)
-      #p 'Output is in rss format'
-    #elsif @output.to_s.downcase == 'json'
-      #ConvertToJson.convert(parsed_data)
-      #p 'Output is in json format'
-    #end
+    converted_data = BaseConverter.convert(parsed_data, @output)
+    FileWriter.write(converted_data, @name)
   end
 end
