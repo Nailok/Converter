a1446c0f039d856a4f9d13fcfdd84310a16f4a0a
Nailok <lexitor34@gmail.com>
2020-03-18 09:54:41 +0300

    Removed file writer

--- lib/converters/modules/convert_to_json.rb
+++ lib/converters/modules/convert_to_json.rb
@@ -5,6 +5,6 @@ require 'json'
 # Class which converts hash data to Json format
 module ConvertToJson
   def self.convert(data)
-    File.open('output.json', 'w+') { |f| f.write JSON.pretty_generate(data) }
+    JSON.pretty_generate(data)
   end
 end
